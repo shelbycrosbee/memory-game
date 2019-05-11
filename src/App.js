afa3d999d34372a6afea43a26b5684d5ac000f92
Shelby Miller <shelbycrosbee@gmail.com>
2019-05-11 17:00:51 -0600

    game thus far

--- src/App.js
+++ src/App.js
@@ -1,48 +1,197 @@
 import React from "react";
 import "./App.css";
+import Card from "./Card";
+import { Z_FILTERED } from "zlib";
+//create a reusable component that is a box
+//create a grid of images/objects add the box component to the 'card'
+//randomize the order of the boxes
+//be able to flip the boxes onClick
+//store the selected box in game
+//add another onClick 
+//store the second selection
+//compare onClick1 to onClick2 if equal setState of grid with those clicks image-up
+//if != reflip go back to previous state
+//continue until all pairs are found
+//create a reset button that returns all of the 'cards' to face down and randomized. 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardOneFlipped: "",
+      clickCounter: 0,
+      cardTwoFlipped: "",
+      cards: [
+        {
+          id: 0,
+          name: "blackPanther",
+          url: "./img/black-panther.jpg",
+          flip: false
+        },
+        {
+          id: 1,
+          name: "blackWidow",
+          url: "./img/black-widow.jpg",
+          flip: false
+        },
+        {
+          id: 2,
+          name: "captainAmerica",
+          url: "./img/captain-america.jpg",
+          flip: false
+        },
+        {
+          id: 3,
+          name: "captainMarvel",
+          url: "./img/captain-marvel.jpg",
+          flip: false
+        },
+        {
+          id: 4,
+          name: "hulk",
+          url: "./img/hulk.jpg",
+          flip: false
+        },
+        {
+          id: 5,
+          name: "ironMan",
+          url: "./img/iron-man.jpg",
+          flip: false
+        },
+        {
+          id: 6,
+          name: "spiderMan",
+          url: "./img/spider-man.jpg",
+          flip: false
+        },
+        {
+          id: 7,
+          name: "thor",
+          url: "./img/thor.jpg",
+          flip: false
+        },
+        {
+          id: 8,
+          name: "xmenRogue",
+          url: "./img/xmen-rogue.jpg",
+          flip: false
+        },
+        {
+          id: 9,
+          name: "xmenWolverine",
+          url: "./img/xmen-wolverine.jpg",
+          flip: false
+        }
+      ]
+    }
+  }
 
-function App() {
-  // Create array to hold all images
-  // Create array which hold tiles which are currently flipped over in current turn
-  // Load images via API
+  componentDidMount() {
+    let idCounter = 10
+    let repeatCardListArr = this.state.cards.map((card) => {
+      let newCard = {
+        ...card,
+        id: idCounter++
+      }
+      return newCard
+    })
 
-  // Push images into array after they are loaded by API
+    var fullCardList = this.state.cards.slice().concat(repeatCardListArr)
 
-  // Create "Tile" Class
-  // Add "imageUrl" string property to Tile class
-  // Add "flipped" boolean to Tile class
+    this.setState({
+      cards: fullCardList,
+    })
+  }
 
-  // Create array of tile objects based on image array
 
-  // Create a "Click" event handler for all tiles
-  // Create a callback function "flipTile" for the event handler
+  handleClick = (e, cardId) => {
+    if (this.state.clickCounter === 0) {
+      let newCards = this.state.cards.map((card) => {
+        if (card.id === cardId) {
+          this.setState({
+            cardOneFlipped: card.name,
+            clickCounter: this.state.clickCounter + 1,
+          })
+          return {
+            ...card,
+            flip: true,
+          };
+        } else {
+          return {
+            ...card
+          }
+        }
+      });
+      this.setState({
+        cards: newCards
+      })
+    }
+    if (this.state.clickCounter === 1) {
+      let newCards = this.state.cards.map((card) => {
+        if (card.id === cardId) {
+          this.setState({
+            cardTwoFlipped: card.name,
+            clickCounter: this.state.clickCounter + 1,
+          })
+          return {
+            ...card,
+            flip: true,
+          };
+        } else {
+          return {
+            ...card
+          }
+        }
+      });
+      this.setState({
+        cards: newCards
+      })
+    }
+    if (this.state.clickCounter === 2) {
+      if (this.state.cardOneFlipped === this.state.cardTwoFlipped) {
+        this.setState({
+          cardOneFlipped: "",
+          cardTwoFlipped: "",
+          clickCounter: 0,
+        })
+      } else {
+        let newCards = this.state.cards.map((card) => {
+          if (card.name === this.state.cardOneFlipped || card.name === this.state.cardTwoFlipped) {
+            return {
+              ...card,
+              flip: false
+            }
+          }
+          else{
+            return {
+              ...card,
+            }
+          }
+        })
+        this.setState({
+          cards: newCards,
+          cardOneFlipped: "",
+          cardTwoFlipped: "",
+          clickCounter: 0,
+        })
+      }
+    }
 
-  // In the "flipTile" function
-  // check if there any tiles that are not flipped over yet (filter function on tiles array)
-  // No -> Game won -> do nothing
-  // Yes -> check if that tile is already image up (check if has class)
-  // Yes -> do nothing
-  // No -> check if already two tiles have been flipped over during this turn (check counter)
-  // No -> flip over tile (set class on image); Push to flippedTiles array; check if two tiles have been flipped over
-  // Yes -> Check if tiles are the same
-  // No -> flip tiles back over and hide images; set counter to 0
-  // Yes -> keep tiles flipped and show images; set counter to 0
-  return (
-    <div>
-      <h1>Memory Game</h1>
-      <p>
-        For instructions check out the corresponding GitHub's repository{" "}
-        <a
-          href="https://github.com/Montana-Code-School/memory-game"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          README.md
-        </a>
-      </p>
-      <p>Happy Problem Solving :)</p>
-    </div>
-  );
+  }
+
+  render() {
+    let cardList = this.state.cards.map((card) => {
+      return <li
+        onClick={e => this.handleClick(e, card.id)}
+        key={card.id}
+        style={{ listStyleType: "none" }}><Card flip={card.flip} imgSrc={card.url} /></li>
+    })
+
+    return (
+      <div>
+        <ul className="gridContainer">{cardList}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Card.css";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+    return (
+          <div className="card">
+            <img src={this.props.imgSrc} className={this.props.flip ? ' smallerImage' : 'smallerImage hidden'} alt="cartoonImage"></img>
+          </div>
+    );
+
+  }
+}
+
+export default Card
